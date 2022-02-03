b6a7cb2cf7b453a8ac9668240aff63d42533f312
DamianGanopolsky <damian98dgg@gmail.com>
2022-02-03 18:15:41 -0300

    add bot commands & guide

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,18 +1,26 @@
-from telegram.ext import CommandHandler
 from telegram import Update
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
 import telegram
-from telegram.ext import Updater
-
 
 
 updater = Updater(token='5164707904:AAFXOrlRZpT1FpfVHPP7ak4QYfC-kafWAvA', use_context=True)
 dispatcher = updater.dispatcher
 
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id,text = "Im a bot")
+    context.bot.send_message(chat_id=update.effective_chat.id,text = "The command is being executed")
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message\
+        (chat_id=update.effective_chat.id, text = """Hi! I'm the NearestCashiersBot.
+        Available commands:
+        /link : lists the 3 nearest Link cashiers
+        /Banelco: lists the 3 nearest Banelco cashiers
+        """
+                                                                )
 
 def run():
     start_handler = CommandHandler('start', start)
+    echo_handler =  MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
     dispatcher.add_handler(start_handler)
     updater.start_polling()
