85eb7f960226370b5b7225979392d203e2f0be61
DamianGanopolsky <damian98dgg@gmail.com>
2022-02-03 19:26:54 -0300

    commands are now insensitive

--- BanelcoCommand.py
+++ BanelcoCommand.py
@@ -1,12 +1,15 @@
 from telegram.ext import MessageFilter
 from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
 from telegram import Update
+import re
 
 class BanelcoHandler(MessageFilter):
     def filter(self,message):
-        return 'banelco'  in message.text
+        return bool(re.search("/banelco", message.text, re.IGNORECASE))
+        #return 'banelco'  in message.text
+
 
     def handlerBanelcoCommand(self,update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="COMMAND LINK")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="COMMAND BANELCO")
 
 
--- LinkCommand.py
+++ LinkCommand.py
@@ -1,10 +1,11 @@
 from telegram.ext import MessageFilter
 from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
 from telegram import Update
+import re
 
 class FilterLink(MessageFilter):
     def filter(self,message):
-        return 'link'  in message.text
+        return bool(re.search("/link", message.text, re.IGNORECASE))
 
     def handlerLinkCommand(self,update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id, text="COMMAND LINK")
