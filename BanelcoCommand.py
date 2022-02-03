dde8267070f5d3e740cf8a4f091f13a1e5057324
DamianGanopolsky <damian98dgg@gmail.com>
2022-02-03 19:19:46 -0300

    add 'banelco' command handler

--- BanelcoCommand.py
+++ BanelcoCommand.py
@@ -1,3 +1,12 @@
+from telegram.ext import MessageFilter
+from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update
 
+class BanelcoHandler(MessageFilter):
+    def filter(self,message):
+        return 'banelco'  in message.text
+
+    def handlerBanelcoCommand(self,update: Update, context: CallbackContext):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="COMMAND LINK")
 
 
--- TelegramBot.py
+++ TelegramBot.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
 import telegram
 from LinkCommand import FilterLink
+from BanelcoCommand import BanelcoHandler
 
 updater = Updater(token='5164707904:AAFXOrlRZpT1FpfVHPP7ak4QYfC-kafWAvA', use_context=True)
 dispatcher = updater.dispatcher
@@ -22,8 +23,11 @@ def run():
     start_handler = CommandHandler('start', start)
     echo_handler =  MessageHandler(Filters.text & (~Filters.command), echo)
     link_filter= FilterLink()
+    banelco_command = BanelcoHandler()
     link_handler = MessageHandler(link_filter,link_filter.handlerLinkCommand)
+    banelco_handler = MessageHandler(banelco_command, banelco_command.handlerBanelcoCommand)
     dispatcher.add_handler(link_handler)
+    dispatcher.add_handler(banelco_handler)
     dispatcher.add_handler(echo_handler)
     dispatcher.add_handler(start_handler)
     updater.start_polling()
