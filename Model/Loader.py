72c655f26e96edb7555ffb6b76bf45b9cd1aa627
DamianGanopolsky <damian98dgg@gmail.com>
2022-02-05 16:25:42 -0300

    add Loader entity to load resources

--- Model/Loader.py
+++ Model/Loader.py
@@ -1,2 +1,58 @@
+import psycopg2
+from config import DATABASE_URL
+import csv
+from Model.Cashier import Cashier
+
+
 class Loader:
-    pass
+
+    def __init__(self):
+        self.banks_initial_extractions = {}
+
+    def __get_data_from_db(self):
+        conn = psycopg2.connect(DATABASE_URL)
+        conn.set_session(autocommit=True)
+
+        cur = conn.cursor()
+
+        cur.execute("""
+            SELECT c.id,c.extractions_done FROM available_cashiers c;
+         """)
+
+        query_result = cur.fetchall()
+
+        for cashier in query_result:
+            self.banks_initial_extractions[cashier[0]] = cashier[1]
+
+        conn.close()
+
+    def __read_file(self, typeOfBank):
+        locations = {}
+        cashiers = []
+        with open('cajeros-automaticos.csv') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            row_count = 0
+
+            for row in csv_reader:
+                row_count += 1
+                if row_count == 1:
+                    continue
+                if row[6] != 'CABA':
+                    continue
+
+                if row[4] != typeOfBank:
+                    continue
+
+                cashier = Cashier(row, self.banks_initial_extractions[int(row[0])])
+
+                cashiers.append(cashier)
+
+                if self.banks_initial_extractions[int(row[0])] >= 1000.0:
+                    continue
+
+                locations.setdefault(cashier.calculate_geohash(), []).append(cashier)
+        return cashiers, locations
+
+    def load_map_resources(self, typeOfBank):
+        self.__get_data_from_db()
+        return self.__read_file(typeOfBank)
--- Model/Map.py
+++ Model/Map.py
@@ -1,17 +1,10 @@
 from ProximityHashes import get_geohashes_neighbours
-import csv
-from Model.Cashier import Cashier
-import psycopg2
-from config import DATABASE_URL
+from Model.Loader import Loader
 
 
 class Map:
     def __init__(self, typeOfBank):
-        self.locations = {}
-        self.cashiers = []
-        self.banks_initial_extractions = {}
-        self.__cashiers_initial_extractions()
-        self.__load(typeOfBank)
+        self.cashiers, self.locations = Loader().load_map_resources(typeOfBank)
 
     def update(self):
         for cashier in self.cashiers:
@@ -27,49 +20,7 @@ class Map:
     def load_cashiers(self):
         for cashier in self.cashiers:
             cashier.load_cashier()
-
         self.__bulk_update()
-
-    def __cashiers_initial_extractions(self):
-        conn = psycopg2.connect(DATABASE_URL)
-        conn.set_session(autocommit=True)
-
-        cur = conn.cursor()
-
-        cur.execute("""
-            SELECT c.id,c.extractions_done FROM available_cashiers c;
-         """)
-
-        query_result = cur.fetchall()
-
-        for cashier in query_result:
-            self.banks_initial_extractions[cashier[0]] = cashier[1]
-
-        conn.close()
-
-    def __load(self, typeOfBank):
-        with open('cajeros-automaticos.csv') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            row_count = 0
-
-            for row in csv_reader:
-                row_count += 1
-                if row_count == 1:
-                    continue
-                if row[6] != 'CABA':
-                    continue
-
-                if row[4] != typeOfBank:
-                    continue
-
-                cashier = Cashier(row, self.banks_initial_extractions[int(row[0])])
-
-                self.cashiers.append(cashier)
-
-                if self.banks_initial_extractions[int(row[0])] >= 1000.0:
-                    continue
-
-                self.locations.setdefault(cashier.calculate_geohash(), []).append(cashier)
 
     def get_nearest_cashiers(self, queryLatitude, queryLongitude):
         proximity_geohashes = get_geohashes_neighbours(queryLatitude, queryLongitude, 500, 7).split(",")
